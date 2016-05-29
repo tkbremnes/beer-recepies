6c6a37dbcf3eba568e0f51ddb54af9d4076e6536
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2016-05-29 19:41:31 +0200

    createComponent helper

---
+++ createComponent.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const fs = require('fs');
+
+if (!process.argv[2]) {
+    // TODO: make it possible to create a name here
+    throw new Error("No component name");
+}
+
+const componentName = process.argv[2];
+
+const template = [
+`import React from 'react';`,
+``,
+`class ${ componentName } extends React.Component {`,
+`    render() {`,
+`        return (`,
+`            `,
+`        )`,
+`    }`,
+`}`,
+``,
+`export default ${ componentName };`
+];
+
+const templateString = (function templateToString(arrayOfStrings) {
+    let res = '';
+    arrayOfStrings.forEach((string) => {
+        res += `${ string }\n`;
+    });
+    return res;
+})(template);
+
+const path = `./scripts/Components/${ componentName }`;
+fs.exists(path, (componentExists) => {
+    if(componentExists) {
+        throw new Error("Component already exists");
+    }
+
+    fs.mkdir(path, () => {
+
+        fs.writeFile(`${ path }/index.jsx`, templateString, (err) => {
+            if (err) {
+                throw new Error('Could not write file');
+            }
+
+            console.log(`${ componentName } created`);
+        });
+    });
+});
