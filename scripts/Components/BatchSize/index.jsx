29b710640a1986ac87ce2507c5b5874e54942906
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2016-05-30 16:50:42 +0200

    editable batch size input field

--- scripts/Components/BatchSize/index.jsx
+++ scripts/Components/BatchSize/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
+let batchSizeValue = 0;
 class BatchSize extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onBatchSizeChange = this.onBatchSizeChange.bind(null, this);
+    }
+
+    onBatchSizeChange(event) {
+        batchSizeValue = event.target.value;
+    }
+
+    componentWillMount() {
+
+    }
+
     render() {
         return (
             <div>
                 <span>Batch size: </span>
-                <input value={ this.props.value }/>
+                <input
+                    value={ batchSizeValue }
+                    onChange={ this.onBatchSizeChange }
+                />
                 <span>L</span>
             </div>
         )
