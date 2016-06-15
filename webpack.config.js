9441f2eb638711ea1e6240b9d518a799eb69e922
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2016-06-15 22:04:16 +0200

    webpack now uses the public folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: './main.js',
   output: {
-      path: __dirname,
+      path: __dirname + '/public/',
       filename: 'bundle.js'
   },
   module: {
