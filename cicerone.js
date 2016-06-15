39742ece4ec16d67d0f1f0d5e29c69c7a539ec97
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2016-06-15 19:24:12 +0200

    removed unused server code

--- cicerone.js
+++
@@ -1,5 +0,0 @@
-const connect = require('connect');
-const serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(80, () => {
-    console.log('Server running on 80...');
-});
